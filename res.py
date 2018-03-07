55bb36f709202e0b9c41ae8f488b054fc7de1458
Vasco-jofra <joaovascofranco@hotmail.com>
2018-03-07 15:17:45 +0000

    Removed garbage

--- res.py
+++
@@ -1,5 +0,0 @@
-[[0;34mINFO[0m] Extracting gadgets for the binary '/bin/ls'
-[[0;34mINFO[0m] Extracting gadgets from the executable section 0x40-0x238
-[[0;34mINFO[0m] Found 0 unique gadgets in 0.01 seconds at depth 10.
-[[0;34mINFO[0m] Skipped 0
-[[0;34mINFO[0m] Extracting gadgets from the executable section 0x0-0x1e4e8
